05bd70d8210af6f3c8cd45226c252d33945b86f0
Harshith Sarabudla <47803152+harshithsarabudla@users.noreply.github.com>
2021-04-13 15:20:58 -0400

    Initial commit

---
+++ Controllers/DepartmentController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private IConfiguration Configuration;
+
+        public DepartmentController(IConfiguration AppConfiguration)
+        {
+            Configuration = AppConfiguration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            
+                SqlCommand command = new SqlCommand(
+                  "SELECT DepartmentId, DepartmentName FROM Department;");
+            
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine("{0}\t{1}", reader.GetInt32(0),
+                        reader.GetString(1));
+                }
+            }
+            else
+            {
+                Console.WriteLine("No rows found.");
+            }
+            reader.Close();
+        }
+
+    }
+}
